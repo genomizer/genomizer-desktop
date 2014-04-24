edfed544ba355b6c0ab136c9b699b831f24b563d
c11dkn <c11dkn@gilderoy.cs.umu.se>
2014-04-24 10:07:15 +0200

    asdf

--- src/genomizerdesktop/Genomizer.java
+++ src/genomizerdesktop/Genomizer.java
@@ -2,14 +2,14 @@ package genomizerdesktop;
 
 public class Genomizer {
 
-	public static void main(String args[]) {
-		System.out.println("Starting Genomizerk");
-		/*Your scrum master was here*/
-		//Lester
+    public static void main(String args[]) {
+	System.out.println("Starting Genomizerk");
+	/* Your scrum master was here */
+	// Lester <- IMPOSTER!!!!!!!
 
-		System.out.println("adam testar");
-		System.out.println("HUR GÅR DET DÅ ADAM /CAPS?");
-		System.out.println("anton e söt //Johan");
-		//Hur blir det nu då!!!?
-	}
+	System.out.println("adam testar");
+	System.out.println("HUR GÅR DET DÅ ADAM /CAPS?");
+	System.out.println("anton e söt //Johan");
+	// Hur blir det nu då!!!?
+    }
 }
