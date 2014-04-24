4ed2829fd7667ed119a01e4727ec03e696f6a9e9
oi11ejn <oi11ejn@cs.umu.se>
2014-04-24 09:15:52 +0200

    lol

--- src/genomizerdesktop/Genomizer.java
+++ src/genomizerdesktop/Genomizer.java
@@ -1,6 +1,10 @@
 package genomizerdesktop;
 
 public class Genomizer {
+	public Genomizer(int nrOfUsers) {
+		this.nrOfUsers = nrOfUsers;
+	}
+
     public static void main(String args[]) {
 	System.out.println("Starting Genomizerk");
     }
