c558b7f295016f3e266f34bc5f9102e1a31f003b
c11dkn <c11dkn@bellatrix.cs.umu.se>
2014-04-24 14:14:56 +0200

    Exempel på gson

--- src/genomizerdesktop/Genomizer.java
+++ src/genomizerdesktop/Genomizer.java
@@ -1,15 +1,23 @@
 package genomizerdesktop;
 
+import com.google.gson.Gson;
+
 public class Genomizer {
 
-	public static void main(String args[]) {
-		System.out.println("Starting Genomizerk");
-		/*Your scrum master was here*/
-		//Lester
+    public static void main(String args[]) {
+	System.out.println("Starting Genomizerk");
+	/* Your scrum master was here */
+	// Lester
 
-		System.out.println("adam testar");
-		System.out.println("HUR GÅR DET DÅ ADAM /CAPS?");
-
-		GUI gui = new GUI();
-	}
+	System.out.println("adam testar");
+	System.out.println("HUR GÅR DET DÅ ADAM /CAPS?");
+	/* Gson exempel */
+	Gson gson = new Gson();
+	LoginCommand login = new LoginCommand("kalle", "123");
+	String json = gson.toJson(login);
+	LoginCommand login2 = gson.fromJson(json, LoginCommand.class);
+	System.out.println(login2.username + " " + login2.password);
+	/* Gson exempel */
+	GUI gui = new GUI();
+    }
 }
