e5bb82e3842a25cb523d408c402b10c472eaceb2
worfox <woffie@worfox.se>
2014-04-24 09:35:48 +0200

    Removed constructor from main class

--- src/genomizerdesktop/Genomizer.java
+++ src/genomizerdesktop/Genomizer.java
@@ -1,14 +1,10 @@
 package genomizerdesktop;
 
 public class Genomizer {
-	public Genomizer(int nrOfUsers) {
-		this.nrOfUsers = nrOfUsers;
-	}
 
 	public static void main(String args[]) {
 		System.out.println("Starting Genomizerk");
 		/*Your scrum master was here*/
-		Genomizer g = new Genomizer(1);
 		//Lester
 	}
 }
