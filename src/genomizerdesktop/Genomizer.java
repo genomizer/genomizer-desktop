808e39fd95fcda6d2b1065ec726eb423a75d111d
c11dkn <c11dkn@gilderoy.cs.umu.se>
2014-04-24 09:04:04 +0200

    zerk

--- src/genomizerdesktop/Genomizer.java
+++ src/genomizerdesktop/Genomizer.java
@@ -1,7 +1,7 @@
 package genomizerdesktop;
 
 public class Genomizer {
-	public static void main(String args[]) {
-		System.out.println("Starting Genomizer");
-	}
+    public static void main(String args[]) {
+	System.out.println("Starting Genomizerk");
+    }
 }
