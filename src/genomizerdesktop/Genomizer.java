e379036accbd34efc86c31d0fda316200039f49a
worfox <woffie@worfox.se>
2014-04-25 15:13:18 +0200

    Merged Genomizer.java

--- src/genomizerdesktop/Genomizer.java
+++ src/genomizerdesktop/Genomizer.java
@@ -1,28 +1,27 @@
 package genomizerdesktop;
 
-import requests.*;
-
 import com.google.gson.Gson;
-import communication.Connection;
+import requests.LoginRequest;
+import requests.Request;
 
 public class Genomizer {
 
     public static void main(String args[]) {
 
-	Gson gson = new Gson();
-	Request login = new LoginRequest("kalle", "123");
-	String json = gson.toJson(login);
+        Gson gson = new Gson();
+        Request login = new LoginRequest("kalle", "123");
+        String json = gson.toJson(login);
 
-	GUI gui = new GUI();
+        GUI gui = new GUI();
         Model model = new Model();
         model.attemptLogin("kalle", "123");
-	SearchTab st = new SearchTab();
-	UploadTab ut = new UploadTab();
+        SearchTab st = new SearchTab();
+        UploadTab ut = new UploadTab();
         WorkspaceTab wt = new WorkspaceTab();
-	gui.setSearchTab(st);
-	gui.setUploadTab(ut);
+        gui.setSearchTab(st);
+        gui.setUploadTab(ut);
         gui.setWorkspaceTab(wt);
-	//Connection con = new Connection("genomizer.apiary-mock.com", 80);
-	//con.sendRequest(login);
+        //Connection con = new Connection("genomizer.apiary-mock.com", 80);
+        //con.sendRequest(login);
     }
 }
