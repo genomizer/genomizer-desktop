34412b561b4317b7e0ef33fcb1ebba8203b322f9
Didrik Püschel <diddemon@hotmail.com>
2014-04-25 14:38:10 +0200

    Upload st00f

--- src/genomizerdesktop/UploadTab.java
+++ src/genomizerdesktop/UploadTab.java
@@ -1,13 +1,26 @@
 package genomizerdesktop;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class UploadTab extends JPanel {
 
+	private static final long serialVersionUID = -2830290705724588252L;
+	private JButton uploadButton;
+	private JTextField directoryTextField;
+	private GridBagLayout gridBag;
+
 	public UploadTab() {
-
-
+		this.setLayout(new BorderLayout());
+		
+		
+		directoryTextField = new JTextField();
+		this.add(directoryTextField, BorderLayout.NORTH);
+		uploadButton = new JButton("UPLOAD");
+		this.add(uploadButton, BorderLayout.CENTER);
 	}
-
-
 }
