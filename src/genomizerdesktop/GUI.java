65b4a65f19a15dba1b1e03d262d840348a1a0c5e
Didrik Püschel <diddemon@hotmail.com>
2014-04-25 09:06:59 +0200

    Placeholder buttons for tabs

--- src/genomizerdesktop/GUI.java
+++ src/genomizerdesktop/GUI.java
@@ -1,21 +1,35 @@
 package genomizerdesktop;
 
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.LayoutManager;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GUI implements GenomizerView {
-	
+
 	private JFrame frame;
-	private JPanel panel;
+	private JPanel mainPanel, tabPanel;
 
 	public GUI() {
 	frame = new JFrame("Genomizer");
-	frame.setSize(400, 800);
-	
-	panel = new JPanel();
-	frame.add(panel);
+	frame.setSize(800, 800);
+
+	BorderLayout bl = new BorderLayout();
+	mainPanel = new JPanel(bl);
+	frame.add(mainPanel);
+
+	tabPanel = new JPanel(new GridBagLayout());
+	mainPanel.add(tabPanel, BorderLayout.NORTH);
+	tabPanel.add(new JButton("SEARCH"));
+	tabPanel.add(new JButton("WORKSPACE"));
+	tabPanel.add(new JButton("ANALYZE"));
+	tabPanel.add(new JButton("PROCESS"));
+	tabPanel.add(new JButton("UPLOAD"));
+
 	frame.setVisible(true);
 	}
 
