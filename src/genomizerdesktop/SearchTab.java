c66bc899000712590e428f370bbadd42aac852e5
oi11ejn <oi11ejn@cs.umu.se>
2014-04-25 16:45:49 +0200

    s

--- src/genomizerdesktop/SearchTab.java
+++ src/genomizerdesktop/SearchTab.java
@@ -10,7 +10,7 @@ import javax.swing.*;
 public class SearchTab extends JPanel {
 
     /**
-	 * 
+	 *
 	 */
 	private static final long serialVersionUID = -5281551884688417212L;
 	private JPanel searchPanel;
@@ -19,9 +19,9 @@ public class SearchTab extends JPanel {
     public SearchTab() {
 	BorderLayout bl = new BorderLayout();
 	setLayout(bl);
-	setBackground(Color.green);
+	setBackground(Color.blue);
 	createSearchPanel();
-	searchPanel.setBackground(Color.red);
+	searchPanel.setBackground(Color.white);
 	add(searchPanel, BorderLayout.NORTH);
     }
 
@@ -37,46 +37,47 @@ public class SearchTab extends JPanel {
 	JLabel expLabel = new JLabel("Experiment ID");
 	JTextField expID = new JTextField();
 
-	String[] petStrings = { "Bird", "Cat", "Dog", "Rabbit", "Pig" };
+	String[] sexStrings = { "Male", "Female" };
+	String[] petStrings1 = { "whole", "Cat", "Dog", "Rabbit", "Pig5" };
+	String[] petStrings2 = { "n", "Cat", "Dog", "Rabbit", "Pig4" };
+	String[] petStrings3 = { "back", "Cat", "Dog", "Rabbit", "Pig3" };
+	String[] petStrings4 = { "m", "Cat", "Dog", "Rabbit", "Pig2" };
+	String[] petStrings5 = { "wrap", "Cat", "Dog", "Rabbit", "Pig1" };
 
-	JComboBox caseCheckBox = new JComboBox(petStrings);
-	JCheckBox wholeCheckBox = new JCheckBox("TEST WHOLE");
-	JCheckBox wrapCheckBox = new JCheckBox("TEST WRAP");
-	JCheckBox backCheckBox = new JCheckBox("BACK TEST");
-	JCheckBox nCheckBox = new JCheckBox("TEST n");
-	JCheckBox mCheckBox = new JCheckBox("m TEST");
+	JComboBox caseCheckBox = new JComboBox(sexStrings);
+	JComboBox wholeCheckBox = new JComboBox(petStrings1);
+	JComboBox wrapCheckBox = new JComboBox(petStrings5);
+	JComboBox backCheckBox = new JComboBox(petStrings3);
+	JComboBox nCheckBox = new JComboBox(petStrings2);
+	JComboBox mCheckBox = new JComboBox(petStrings4);
 	JButton searchButton = new JButton("Search");
 //	JButton cancelButton = new JButton("Cancel");
 
-	gl.setHorizontalGroup(gl.createSequentialGroup()
-		    .addComponent(searchLabel)
-		    .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-		        .addComponent(expID)
-		        .addGroup(gl.createSequentialGroup()
-		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-		                .addComponent(caseCheckBox)
+	gl.setHorizontalGroup(gl.createParallelGroup()
+		.addComponent(searchLabel)
+		.addComponent(expID)
+		.addGroup(gl.createSequentialGroup()
+			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+				.addComponent(caseCheckBox)
 		                .addComponent(wholeCheckBox))
-		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-		                .addComponent(wrapCheckBox)
+		        .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        	.addComponent(wrapCheckBox)
 		                .addComponent(backCheckBox))
-		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
 		                .addComponent(nCheckBox)
-		                .addComponent(mCheckBox))))
-		     .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-			     .addComponent(searchButton))
+		                .addComponent(mCheckBox)))
+		 .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
+		        	.addComponent(searchButton))
 		);
 	gl.linkSize(SwingConstants.HORIZONTAL, searchButton);
 
 	gl.setVerticalGroup(gl.createSequentialGroup()
 		.addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-    			.addComponent(searchLabel)
-    			.addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-    		        .addGroup(gl.createSequentialGroup()
-    		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-    		        .addComponent(expID)
-    		    .addComponent(searchButton))
+    		        .addComponent(searchLabel))
     		    .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-    		        .addGroup(gl.createSequentialGroup()
+   		        .addGroup(gl.createSequentialGroup()
+   	    		     .addComponent(expID)
+   	    		     .addComponent(searchButton)
     		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
     		                .addComponent(caseCheckBox)
     		                .addComponent(wrapCheckBox)
@@ -84,47 +85,18 @@ public class SearchTab extends JPanel {
     		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
     		                .addComponent(wholeCheckBox)
     		                .addComponent(backCheckBox)
-    		                .addComponent(mCheckBox)))))))
+    		                .addComponent(mCheckBox))))
     		);
-
-//	gl.setHorizontalGroup(gl.createSequentialGroup()
-//		    .addComponent(searchLabel)
-//		    .addComponent(expLabel)
-//		    .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-//		        .addComponent(expID)
-//		        .addGroup(gl.createSequentialGroup()
-//		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-//		                .addComponent(caseCheckBox)
-//		                .addComponent(wholeCheckBox))
-//		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-//		                .addComponent(wrapCheckBox)
-//		                .addComponent(backCheckBox))))
-//		    .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-//		        .addComponent(searchButton))
-//		);
-//		gl.linkSize(SwingConstants.HORIZONTAL, searchButton);
-//
-//		gl.setVerticalGroup(gl.createSequentialGroup()
-//		    .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-//		        .addComponent(searchLabel)
-//		        .addComponent(expLabel)
-//		        .addComponent(expID)
-//		        .addComponent(searchButton))
-//		    .addGroup(gl.createParallelGroup(GroupLayout.Alignment.LEADING)
-//		        .addGroup(gl.createSequentialGroup()
-//		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-//		                .addComponent(caseCheckBox)
-//		                .addComponent(wrapCheckBox))
-//		            .addGroup(gl.createParallelGroup(GroupLayout.Alignment.BASELINE)
-//		                .addComponent(wholeCheckBox)
-//		                .addComponent(backCheckBox))))
-//		);
     }
 
     public static void main(String args[]) {
 	GUI g = new GUI();
 	SearchTab st = new SearchTab();
+	UploadTab ut = new UploadTab();
+	WorkspaceTab wt = new WorkspaceTab();
 	g.setSearchTab(st);
+	g.setUploadTab(ut);
+	g.setWorkspaceTab(wt);
     }
 }
 
