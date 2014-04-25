b4a5a10a1f6c9f16c00de2196a0fdfc3c2ca48ab
c11dkn <c11dkn@gryffindor.cs.umu.se>
2014-04-25 11:05:27 +0200

    update tests

--- tests/requestTests/DownloadFileRequestTest.java
+++ tests/requestTests/DownloadFileRequestTest.java
@@ -3,33 +3,37 @@ package requestTests;
 import static org.fest.assertions.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import requests.DownloadFileRequest;
 
 public class DownloadFileRequestTest {
 
+    private DownloadFileRequest download;
+
+    @Before
+    public void setUp() {
+	download = new DownloadFileRequest("file1", "wig");
+    }
+
     @Test
     public void testCreateDownloadRequest() {
-	DownloadFileRequest download = new DownloadFileRequest("file1", "wig");
 	assertThat(download).isNotNull();
     }
 
     @Test
     public void testGetFileName() {
-	DownloadFileRequest download = new DownloadFileRequest("file1", "wig");
 	assertEquals("file1", download.fileName);
     }
 
     @Test
     public void testGetFileFormat() {
-	DownloadFileRequest download = new DownloadFileRequest("file1", "wig");
 	assertEquals("wig", download.fileFormat);
     }
 
     @Test
     public void testGetRequestName() {
-	DownloadFileRequest download = new DownloadFileRequest("file1", "wig");
 	assertEquals("downloadfile", download.requestName);
     }
 
--- tests/requestTests/LoginRequestTest.java
+++ tests/requestTests/LoginRequestTest.java
@@ -3,33 +3,37 @@ package requestTests;
 import static org.fest.assertions.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import requests.LoginRequest;
 
 public class LoginRequestTest {
 
+    private LoginRequest login;
+
+    @Before
+    public void setUp() {
+	login = new LoginRequest("Kalle", "123");
+    }
+
     @Test
     public void testCreateLoginRequest() {
-	LoginRequest login = new LoginRequest("Kalle", "123");
 	assertThat(login).isNotNull();
     }
 
     @Test
     public void testGetUsername() {
-	LoginRequest login = new LoginRequest("Kalle", "123");
 	assertEquals("Kalle", login.username);
     }
 
     @Test
     public void testGetPassword() {
-	LoginRequest login = new LoginRequest("Kalle", "123");
 	assertEquals("123", login.password);
     }
-    
+
     @Test
     public void testGetRequestName() {
-	LoginRequest login = new LoginRequest("Kalle", "123");
 	assertEquals("login", login.requestName);
     }
 
