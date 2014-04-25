82888d77cd7785b56bde26c1e4feebd0ab08f279
c11agn <c11agn@mcgonagall.cs.umu.se>
2014-04-25 08:47:01 +0200

    centrerat gui

--- src/genomizerdesktop/GUI.java
+++ src/genomizerdesktop/GUI.java
@@ -6,16 +6,17 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class GUI implements GenomizerView {
-	
+
 	private JFrame frame;
 	private JPanel panel;
 
 	public GUI() {
 	frame = new JFrame("Genomizer");
 	frame.setSize(400, 800);
-	
+
 	panel = new JPanel();
 	frame.add(panel);
+	frame.setLocationRelativeTo(null);
 	frame.setVisible(true);
 	}
 
