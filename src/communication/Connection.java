ccbbb404a5ed0aae5103484b47767912e73e4703
c11dkn <c11dkn@gryffindor.cs.umu.se>
2014-04-25 09:19:17 +0200

    added request tests

--- src/communication/Connection.java
+++ src/communication/Connection.java
@@ -1,7 +1,6 @@
 package communication;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -10,76 +9,76 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.LinkedList;
 
-import requests.*;
+import requests.Request;
 
 public class Connection implements Runnable {
-	private String ip;
-	private int port;
-	private Socket socket;
-	private LinkedList<String> incomingMessages = new LinkedList<String>();
-	private boolean running = true;
+    private String ip;
+    private int port;
+    private Socket socket;
+    private LinkedList<String> incomingMessages = new LinkedList<String>();
+    private boolean running = true;
 
-	public Connection(String ip, int port) {
-		this.ip = ip;
-		this.port = port;
-		setupNetwork();
+    public Connection(String ip, int port) {
+	this.ip = ip;
+	this.port = port;
+	setupNetwork();
+    }
+
+    public void setupNetwork() {
+	try {
+	    socket = new Socket(ip, port);
+	} catch (UnknownHostException e) {
+	    System.out.println("Unknown host: " + ip);
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
 	}
+    }
 
-	public void setupNetwork() {
-		try {
-			socket = new Socket(ip, port);
-		} catch (UnknownHostException e) {
-			System.out.println("Unknown host: " + ip);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+    public boolean sendRequest(Request request) {
+	try {
+	    PrintWriter out = new PrintWriter(new OutputStreamWriter(
+		    socket.getOutputStream()), true);
+	    System.out.println(request.toJson());
+	    out.println(request.toJson());
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+	return true;
+    }
+
+    public void stopListening() {
+	running = false;
+	try {
+	    socket.close();
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+    }
+
+    @Override
+    public void run() {
+	try {
+	    BufferedReader in = new BufferedReader(new InputStreamReader(
+		    socket.getInputStream()));
+
+	    while (running) {
+		String message = in.readLine();
+		if (message == null) {
+		    running = false;
+		    break;
 		}
+		System.out.println(message);
+		incomingMessages.add(message);
+		System.out.println(incomingMessages);
+
+	    }
+	} catch (IOException e) {
+	    // TODO Auto-generated catch block
+	    running = false;
+	    e.printStackTrace();
 	}
-
-	public boolean sendRequest(Request request) {
-		try {
-			PrintWriter out = new PrintWriter(new OutputStreamWriter(
-					socket.getOutputStream()), true);
-			System.out.println(request.toJson());
-			out.println(request.toJson());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return true;
-	}
-
-	public void stopListening() {
-		running = false;
-		try {
-			socket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void run() {
-		try {
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					socket.getInputStream()));
-
-			while (running) {
-				String message = in.readLine();
-				if(message == null) {
-					running = false;
-					break;
-				}
-				System.out.println(message);
-				incomingMessages.add(message);
-				System.out.println(incomingMessages);
-
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			running = false;
-			e.printStackTrace();
-		}
-	}
+    }
 }
