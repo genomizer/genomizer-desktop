dcec81dc41d64f3b03c09b54b0add7a21a8a8a43
c11dkn <c11dkn@bellatrix.cs.umu.se>
2014-04-28 08:47:27 +0200

    fds

---
+++ src/Model.java
@@ -0,0 +1,20 @@
+package genomizerdesktop;
+
+public class Model {
+
+    private String userID = "";
+
+    // workspafdfdfdcess??
+
+    public Model() {
+    }
+
+    public String getUserID() {
+	return userID;
+    }
+
+    public void setUserID(String userID) {
+	this.userID = userID;
+    }
+
+}
