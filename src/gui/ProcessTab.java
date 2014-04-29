8de20c4e7291a8adc7a4e90ca9db32b00c3bf9fa
c11ann <c11ann@weasley.cs.umu.se>
2014-04-29 10:02:21 +0200

    dfs

--- src/gui/ProcessTab.java
+++ src/gui/ProcessTab.java
@@ -121,16 +121,17 @@ public class ProcessTab extends JPanel {
 		procQueuePanel.add(queueLabel);
 
 	}
-	
+
 	public void addCheckbox(JCheckBox checkBox) {
-		
-	    ListModel currentList = this.getModel();
+
+	 /*   ListModel currentList = this.getModel();
 	    JCheckBox[] newList = new JCheckBox[currentList.getSize() + 1];
 	    for (int i = 0; i < currentList.getSize(); i++) {
 	        newList[i] = (JCheckBox) currentList.getElementAt(i);
 	    }
 	    newList[newList.length - 1] = checkBox;
 	    setListData(newList);
+	*/
 	}
 
 }
