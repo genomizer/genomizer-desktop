0aacc45d94b37edbef58617bbd143fd59b508a53
Didrik Püschel <diddemon@hotmail.com>
2014-04-30 10:39:07 +0200

    DownloadWindow fixes..

--- src/gui/DownloadWindow.java
+++ src/gui/DownloadWindow.java
@@ -2,9 +2,11 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Vector;
 
+import javax.swing.DefaultCellEditor;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -12,6 +14,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
+import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumn;
 import javax.swing.table.TableModel;
@@ -23,41 +26,54 @@ public class DownloadWindow extends JFrame {
 	JTable table;
 
 	//Tar in ArrayList med de filer som valdes
-	public DownloadWindow(String[] files) {
+	public DownloadWindow(ArrayList<String> files) {
 
 		setUp(files);
 	}
 
 	public DownloadWindow() {
-		String[] data = {
-	    	 "Protein123_A5_2014.WIG"  ,
-	    	 "Protein123_A5_2014.RAW"  ,
-	    	 "Protein123_A5_2014.WIG"   };
+		ArrayList<String> data = new ArrayList<String>();
+
+		data.add("Protein123_A5_2014.WIG");
+		data.add("Protein123_A5_2014.RAW");
+		data.add("Protein123_A5_2014.WIG");
 
 		setUp(data);
 	}
 
-	private void setUp(String[] data) {
+	private void setUp(ArrayList<String> data) {
 
 		panel = new JPanel(new BorderLayout(3,3));
 		add(panel);
 		panel.add(new JLabel("test"), BorderLayout.NORTH);
-		
+
 		DefaultTableModel tableModel = new DefaultTableModel();
 		table = new JTable(tableModel);
-		
+
 		TableColumn fileNameColumn, formatConversionColumn;
 		fileNameColumn = new TableColumn();
 		formatConversionColumn = new TableColumn();
 		fileNameColumn.setHeaderValue("File name");
 		formatConversionColumn.setHeaderValue("Format conversion");
-		table.addColumn(fileNameColumn);
-		table.addColumn(formatConversionColumn);
 		
-		for(int i=0; i<data.length; i++) {
-			table.add(new JLabel(data[i]));
+		tableModel.addColumn(fileNameColumn);
+		tableModel.addColumn(formatConversionColumn);
+
+		for(int i=0; i<data.size(); i++) {
+			
+			tableModel.addRow(new Object[]{data.get(i),null});
+			System.out.println(tableModel.getValueAt(i, 0));
+			System.out.println(tableModel.getValueAt(i, 1));
+			System.out.println("   number of rows: "+tableModel.getRowCount());
 		}
 		
+		JComboBox comboBox = new JComboBox();
+		comboBox.addItem("Format1");
+		comboBox.addItem("Format2");
+		comboBox.addItem("Format3");
+		comboBox.setMinimumSize(new Dimension(70,20));
+		formatConversionColumn.setCellEditor(new DefaultCellEditor(comboBox));
+
     	table.setBackground(Color.blue);
     	table.setRowHeight(30);
 
