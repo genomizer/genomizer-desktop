1222735a0539d8751033a0bf7630467e281f9f3c
c11ann <c11ann@headless-monk.cs.umu.se>
2014-05-02 10:51:06 +0200

    Snyggat till processtab

--- src/gui/ProcessTab.java
+++ src/gui/ProcessTab.java
@@ -27,6 +27,7 @@ public class ProcessTab extends JPanel {
 
 	private static final long serialVersionUID = -2830290705724588252L;
 
+	private JTextArea timeArea = new JTextArea();
 	private JPanel filesPanel = new JPanel();
 	private JPanel scheduleProcPanel = new JPanel();
 	private JPanel genProfileDataPanel = new JPanel();
@@ -41,17 +42,12 @@ public class ProcessTab extends JPanel {
 	private JButton profileButton = new JButton("CREATE PROFILE DATA");
 	private JButton regionButton = new JButton("CREATE REGION DATA");
 	private JButton scheduleButton = new JButton("SCHEDULE");
-
-	//SKA VARA JLIST
+	private JScrollPane scrollFiles = new JScrollPane();
+	private DefaultListModel fileListModel = new DefaultListModel();
+	private JList fileList = new JList();
+	//TODO SKA VARA JLIST
 	private ArrayList processQueue = new ArrayList();
 
-	private DefaultListModel fileListModel = new DefaultListModel();
-	//private JCheckBoxList fileList = new JCheckBoxList();
-	private JList fileList = new JList();
-
-	private JScrollPane scrollFiles = new JScrollPane();
-
-	private ArrayList<JCheckBox> arrayCheck = new ArrayList<JCheckBox>();
 
 	public ProcessTab(){
 		this.setLayout(new BorderLayout());
@@ -65,11 +61,18 @@ public class ProcessTab extends JPanel {
 		initMiddlePanel();
 		initEastPanels();
 		initTimePanel();
+
+		//BARA TILLFÄLLIG
+		JList list = new JList(new CheckListItem[] {
+        		new CheckListItem("[0] Protein223_A5_2014.RAW"),
+                new CheckListItem("[1] Protein223_A5_2014.RAW"),
+                new CheckListItem("[2] Protein223_A5_2014.RAW"),
+                new CheckListItem("[3] Protein223_A5_2014.RAW"),
+                new CheckListItem("[4] Protein223_A5_2014.RAW")});
+
+		initList(list);
 		initFileList();
-
-		test();
-
-		printTimePanel();
+		writeToTimePanel();
 
 	}
 
@@ -77,65 +80,122 @@ public class ProcessTab extends JPanel {
 
 		this.add(menuPanel,BorderLayout.NORTH);
 		menuPanel.setPreferredSize(new Dimension(300,100));
-
-		menuPanel.add(convertButton);
-		menuPanel.add(profileButton);
-		menuPanel.add(regionButton);
-		menuPanel.add(scheduleButton);
-
-		convertButton.setEnabled(false);
-		regionButton.setEnabled(false);
-		scheduleButton.setEnabled(false);
-
-	}
-
-	private void initTimePanel() {
-		this.add(timePanel,BorderLayout.SOUTH);
-		timePanel.setPreferredSize(new Dimension(300,30));
-
-	}
-
-	private void printTimePanel(){
-
-		JTextArea timeArea = new JTextArea();
-		timeArea.setText("");
-		timeArea.append("Number of jobs currently in queue: " + getNumberOfJobsInQueue() + " (est. time until empty : " + getTimeApprox() + " min )");
-		timeArea.setEditable(false);
-
-
-		timePanel.add(timeArea);
+		addButtonsToMenu();
+		enableButtons();
 
 	}
 
 	private void initWestPanels(){
 
 		this.add(leftPanel,BorderLayout.WEST);
-
-		leftPanel.add(filesPanel);
-		//filesPanel.setBorder( BorderFactory.createTitledBorder("FILES"));
 		filesPanel.setPreferredSize(new Dimension(300,100));
-
-		leftPanel.add(scheduleProcPanel);
-		scheduleProcPanel.setBorder( BorderFactory.createTitledBorder("SCHEDULE PROCESSING"));
-		scheduleProcPanel.setPreferredSize(new Dimension(300,100));
+		initLeftPanel();
+		initScheduleProcPanel();
 
 	}
 
-	private void initFileList(){
+	private void initMiddlePanel(){
 
-        fileList = new JList(new CheckListItem[] {
-        		new CheckListItem("[0] Protein223_A5_2014.RAW"),
-                new CheckListItem("[1] Protein223_A5_2014.RAW"),
-                new CheckListItem("[2] Protein223_A5_2014.RAW"),
-                new CheckListItem("[3] Protein223_A5_2014.RAW"),
-                new CheckListItem("[4] Protein223_A5_2014.RAW")});
+		this.add(middelPanel,BorderLayout.CENTER);
+		initGenProfileDataPanel();
+		initGenRegionDataPanel();
+		initConvertFilesPanel();
 
-          fileList.setCellRenderer(new CheckListRenderer());
+	}
+
+	private void initEastPanels(){
+		this.add(procQueuePanel,BorderLayout.EAST);
+		procQueuePanel.setPreferredSize(new Dimension(300,100));
+		initProcessInQueue();
+
+	}
+
+	private void initConvertFilesPanel() {
+		middelPanel.add(convertFilesPanel);
+		convertFilesPanel.setBorder( BorderFactory.createTitledBorder("CONVERT FILES"));
+	}
+
+	private void initGenRegionDataPanel() {
+		middelPanel.add(genRegionDataPanel);
+		genRegionDataPanel.setBorder( BorderFactory.createTitledBorder("GENERATE REGION DATA"));
+	}
+
+	private void initGenProfileDataPanel() {
+		middelPanel.add(genProfileDataPanel);
+		genProfileDataPanel.setBorder( BorderFactory.createTitledBorder("GENERATE PROFILE DATA"));
+	}
+
+	private void initProcessInQueue() {
+		JLabel queueLabel = new JLabel("PROCESSING IN QUEUE");
+		queueLabel.setOpaque(true);
+		procQueuePanel.add(queueLabel);
+	}
+
+	private void initTimePanel() {
+		this.add(timePanel,BorderLayout.SOUTH);
+		timePanel.setPreferredSize(new Dimension(300,30));
+	}
+
+	private void initScheduleProcPanel() {
+		scheduleProcPanel.setBorder( BorderFactory.createTitledBorder("SCHEDULE PROCESSING"));
+		scheduleProcPanel.setPreferredSize(new Dimension(300,100));
+	}
+
+	private void initLeftPanel() {
+		leftPanel.add(filesPanel);
+		leftPanel.add(scheduleProcPanel);
+	}
+
+	private void addButtonsToMenu() {
+		menuPanel.add(convertButton);
+		menuPanel.add(profileButton);
+		menuPanel.add(regionButton);
+		menuPanel.add(scheduleButton);
+	}
+
+	private void enableButtons() {
+		convertButton.setEnabled(false);
+		regionButton.setEnabled(false);
+		scheduleButton.setEnabled(false);
+	}
+
+	private void writeToTimePanel(){
+
+		timeArea.setText("");
+		timeArea.setEditable(false);
+		timeArea.append("Number of jobs currently in queue: " + getNumberOfJobsInQueue() + " (est. time until empty : " + getTimeApprox() + " min )");
+		timePanel.add(timeArea);
+
+	}
+
+	private void initList(JList fileList){
+		this.fileList = fileList;
+	}
+
+	private void fileListSetCellRenderer() {
+		fileList.setCellRenderer(new CheckListRenderer());
           fileList.setSelectionMode(
              ListSelectionModel.SINGLE_SELECTION);
+	}
 
 
-          fileList.addMouseListener(new MouseAdapter()
+	private void initFileList(){
+
+        fileListSetCellRenderer();
+        fileListAddMouseListener();
+        initScrollPane();
+	}
+
+	private void initScrollPane() {
+		scrollFiles = new JScrollPane( fileList );
+        scrollFiles.setBorder(BorderFactory.createTitledBorder("FILES"));
+        fileList.setFixedCellWidth(265);
+        fileList.setFixedCellHeight(40);
+        filesPanel.add(scrollFiles);
+	}
+
+	private void fileListAddMouseListener() {
+		fileList.addMouseListener(new MouseAdapter()
           {
              public void mouseClicked(MouseEvent event)
              {
@@ -151,40 +211,6 @@ public class ProcessTab extends JPanel {
                 list.repaint(list.getCellBounds(index, index));
              }
           });
-
-        scrollFiles = new JScrollPane( fileList );
-
-        scrollFiles.setBorder(BorderFactory.createTitledBorder("FILES"));
-        fileList.setFixedCellWidth(265);
-        fileList.setFixedCellHeight(40);
-
-        filesPanel.add(scrollFiles);
-	}
-
-	private void initMiddlePanel(){
-
-
-		this.add(middelPanel,BorderLayout.CENTER);
-
-		middelPanel.add(genProfileDataPanel);
-		genProfileDataPanel.setBorder( BorderFactory.createTitledBorder("GENERATE PROFILE DATA"));
-
-		middelPanel.add(genRegionDataPanel);
-		genRegionDataPanel.setBorder( BorderFactory.createTitledBorder("GENERATE REGION DATA"));
-
-		middelPanel.add(convertFilesPanel);
-		convertFilesPanel.setBorder( BorderFactory.createTitledBorder("CONVERT FILES"));
-
-	}
-
-	private void initEastPanels(){
-		this.add(procQueuePanel,BorderLayout.EAST);
-		procQueuePanel.setPreferredSize(new Dimension(300,100));
-
-		JLabel queueLabel = new JLabel("PROCESSING IN QUEUE");
-		queueLabel.setOpaque(true);
-		procQueuePanel.add(queueLabel);
-
 	}
 
 	public ArrayList<String> getAllMarkedFiles(){
@@ -193,12 +219,16 @@ public class ProcessTab extends JPanel {
 
 		for(int i = 0; i < fileList.getModel().getSize();i++){
 			CheckListItem checkItem = (CheckListItem) fileList.getModel().getElementAt(i);
-
-			if(checkItem.isSelected()){
-				arr.add(checkItem.toString());
-			}
+			checkItemIsSelected(arr, checkItem);
 		}
 		return arr;
+	}
+
+	private void checkItemIsSelected(ArrayList<String> arr,
+			CheckListItem checkItem) {
+		if(checkItem.isSelected()){
+			arr.add(checkItem.toString());
+		}
 	}
 
 	public void addFileListMouseListener(MouseAdapter mouseAdapter){
@@ -225,16 +255,9 @@ public class ProcessTab extends JPanel {
 		return this.processQueue.size();
 	}
 
+	//TODO Returnera den riktiga tidsapproxen
 	private int getTimeApprox(){
 		return 450;
-	}
-
-	private void test() {
-		//Test
-		for(int i = 0; i < 12; i++){
-			processQueue.add("Elem" + i);
-		}
-		//
 	}
 
 }
