d7abfa4c4d650fb35d73e8ef543170e2986973bc
oi11ahn <oi11ahn@voldemort.cs.umu.se>
2014-05-06 10:57:50 +0200

    Can now add buttons dynamically at sidebar in sysadmin tab

--- src/gui/sysadmin/SysadminTab.java
+++ src/gui/sysadmin/SysadminTab.java
@@ -5,16 +5,12 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -28,237 +24,248 @@ import javax.swing.table.TableRowSorter;
 
 public class SysadminTab extends JPanel {
 
-	private TableRowSorter<TableModel> rowSorter;
-	private SysadminAnnotationPopup pop;
+    private static final String[] buttonNames = new String[] { "Annotations",
+            "Users", "TestMe" };
 
-	private static final long serialVersionUID = 3718367832670081148L;
-	private JButton addButton;
+    private TableRowSorter<TableModel> rowSorter;
+    private SysadminAnnotationPopup pop;
 
-	/**
-	 * Create the panel.
-	 */
-	public SysadminTab() {
-		setLayout(new BorderLayout());
-		buildMainPanel();
-		buildSidePanel();
+    private static final long serialVersionUID = 3718367832670081148L;
+    private JButton addButton;
 
-	}
+    /**
+     * Create the panel.
+     */
+    public SysadminTab() {
+        setLayout(new BorderLayout());
+        buildMainPanel();
+        buildSidePanel(buttonNames);
 
-	private void buildMainPanel() {
+    }
 
-		JPanel mainPanel = new JPanel();
-		JPanel searchPanel = new JPanel(new FlowLayout());
-		JPanel bottomPanel = new JPanel(new BorderLayout());
-		JPanel tabPanel = new JPanel();
-		JPanel buttonPanel = new JPanel();
+    private void buildMainPanel() {
 
-		buildTabPanel(tabPanel);
-		buildSearchPanel(searchPanel);
-		buildButtonPanel(buttonPanel);
-		buildBottomPanel(bottomPanel, tabPanel, buttonPanel);
+        JPanel mainPanel = new JPanel();
+        JPanel searchPanel = new JPanel(new FlowLayout());
+        JPanel bottomPanel = new JPanel(new BorderLayout());
+        JPanel tabPanel = new JPanel();
+        JPanel buttonPanel = new JPanel();
 
-		mainPanel.setLayout(new BorderLayout(0, 0));
-		mainPanel.add(searchPanel, BorderLayout.NORTH);
-		mainPanel.add(bottomPanel);
+        buildTabPanel(tabPanel);
+        buildSearchPanel(searchPanel);
+        buildButtonPanel(buttonPanel);
+        buildBottomPanel(bottomPanel, tabPanel, buttonPanel);
 
-		add(mainPanel, BorderLayout.CENTER);
-	}
+        mainPanel.setLayout(new BorderLayout(0, 0));
+        mainPanel.add(searchPanel, BorderLayout.NORTH);
+        mainPanel.add(bottomPanel);
 
-	private void buildBottomPanel(JPanel bottomPanel, JPanel tabPanel,
-			JPanel buttonPanel) {
-		bottomPanel.setBackground(new Color(255, 200, 200));
-		bottomPanel.add(buttonPanel, BorderLayout.EAST);
-		bottomPanel.add(tabPanel, BorderLayout.CENTER);
-	}
+        add(mainPanel, BorderLayout.CENTER);
+    }
 
-	private void buildTabPanel(JPanel tabPanel) {
-		tabPanel.setBackground(new Color(255, 250, 250));
-		tabPanel.setLayout(new GridLayout(1, 0));
-		tabPanel.add(buildMockTable());
+    private void buildBottomPanel(JPanel bottomPanel, JPanel tabPanel,
+            JPanel buttonPanel) {
+        bottomPanel.setBackground(new Color(255, 200, 200));
+        bottomPanel.add(buttonPanel, BorderLayout.EAST);
+        bottomPanel.add(tabPanel, BorderLayout.CENTER);
+    }
 
-	}
+    private void buildTabPanel(JPanel tabPanel) {
+        tabPanel.setBackground(new Color(255, 250, 250));
+        tabPanel.setLayout(new GridLayout(1, 0));
+        tabPanel.add(buildMockTable());
 
-	private void buildButtonPanel(JPanel buttonPanel) {
-		buttonPanel.setBackground(new Color(215, 200, 200));
-		buttonPanel.setLayout(new GridLayout(20, 1));
-		JButton modifyButton = new JButton("Modify"); // TODO: load from a list
-		addButton = new JButton("Add");
-		JButton removeButton = new JButton("Remove");
-		buttonPanel.add(modifyButton);
-		buttonPanel.add(addButton);
-		buttonPanel.add(removeButton);
-	}
+    }
 
-	private void buildSearchPanel(JPanel searchPanel) {
-		JTextField searchField = buildSearchField();
-		searchPanel.setLayout(new BorderLayout());
-		JPanel paneception = new JPanel(new GridLayout(1, 1));
+    private void buildButtonPanel(JPanel buttonPanel) {
+        buttonPanel.setBackground(new Color(215, 200, 200));
+        buttonPanel.setLayout(new GridLayout(20, 1));
+        JButton modifyButton = new JButton("Modify"); // TODO: load from a list
+        addButton = new JButton("Add");
+        JButton removeButton = new JButton("Remove");
+        buttonPanel.add(modifyButton);
+        buttonPanel.add(addButton);
+        buttonPanel.add(removeButton);
+    }
 
-		JButton searchButton = new JButton("Search");
+    private void buildSearchPanel(JPanel searchPanel) {
+        JTextField searchField = buildSearchField();
+        searchPanel.setLayout(new BorderLayout());
+        JPanel paneception = new JPanel(new GridLayout(1, 1));
 
-		// searchPanel.setPreferredSize(new Dimension(600, 40));
-		searchPanel.setBackground(new Color(245, 245, 245));
-		searchPanel.add(searchField, BorderLayout.CENTER);
-		paneception.add(searchButton);
-		searchPanel.add(paneception, BorderLayout.EAST);
-	}
+        JButton searchButton = new JButton("Search");
 
-	private JTextField buildSearchField() {
-		Dimension searchDim = new Dimension(500, 30);
-		JTextField searchField = new JTextField("Search...");
-		searchField.getDocument().addDocumentListener(
-				new SearchDocumentListener(rowSorter, searchField));
-		searchField.setPreferredSize(searchDim);
-		return searchField;
-	}
+        // searchPanel.setPreferredSize(new Dimension(600, 40));
+        searchPanel.setBackground(new Color(245, 245, 245));
+        searchPanel.add(searchField, BorderLayout.CENTER);
+        paneception.add(searchButton);
+        searchPanel.add(paneception, BorderLayout.EAST);
+    }
 
-	private void buildSidePanel() {
-		JPanel sidePanel = new JPanel();
-		sidePanel.setLayout(new BoxLayout(sidePanel, 1));
-		sidePanel.add(new JButton("Annotations")); // TODO: load from a list of
-													// function
-		add(sidePanel, BorderLayout.WEST);
-	}
+    private JTextField buildSearchField() {
+        Dimension searchDim = new Dimension(500, 30);
+        JTextField searchField = new JTextField("Search...");
+        searchField.getDocument().addDocumentListener(
+                new SearchDocumentListener(rowSorter, searchField));
+        searchField.setPreferredSize(searchDim);
+        return searchField;
+    }
 
-	private Component buildMockTable() {
-		JPanel panel = new JPanel(new BorderLayout());
+    private void buildSidePanel(String[] buttonNames) {
 
-		String[] attributes = { "Name", "Type", "Forced" };
+        JPanel sidePanel = new JPanel();
+        sidePanel.setLayout(new BoxLayout(sidePanel, 1));
 
-		Object[][] data = { { "", "", "" }, { "", "", "" } };
+        for (int i = 0; i < buttonNames.length; i++) {
 
-		JTable table = new JTable(data, attributes);
-		table.setPreferredSize(panel.getSize());
+            sidePanel.add(new JButton(buttonNames[i]));
 
-		table.setShowGrid(false);
-		JTableHeader header = table.getTableHeader();
+        }
 
-		TableModel tablemodel = table.getModel();
+        // sidePanel.add(new JButton("Annotations")); // TODO: load from a list
+        // of
+        // function
+        add(sidePanel, BorderLayout.WEST);
+    }
 
-		panel.add(header, BorderLayout.NORTH);
-		panel.add(table, BorderLayout.CENTER);
+    private Component buildMockTable() {
+        JPanel panel = new JPanel(new BorderLayout());
 
-		JScrollPane scroll = new JScrollPane(panel);
-		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        String[] attributes = { "Name", "Type", "Forced" };
 
-		TableRowSorter<TableModel> rowSorter = new TableRowSorter<TableModel>(
-				tablemodel);
-		this.rowSorter = rowSorter;
-		table.setRowSorter(rowSorter);
+        Object[][] data = { { "", "", "" }, { "", "", "" } };
 
-		return scroll;
-	}
+        JTable table = new JTable(data, attributes);
+        table.setPreferredSize(panel.getSize());
 
-	private Component buildSearchTable() {
+        table.setShowGrid(false);
+        JTableHeader header = table.getTableHeader();
 
-		JPanel panel = new JPanel(new BorderLayout());
-		TableModel tableModel = null; // TODO: fix this add a new class?
-		JTable table = new JTable(tableModel);
-		table.setPreferredSize(panel.getSize());
+        TableModel tablemodel = table.getModel();
 
-		table.setShowGrid(false);
+        panel.add(header, BorderLayout.NORTH);
+        panel.add(table, BorderLayout.CENTER);
 
-		JTableHeader header = table.getTableHeader();
+        JScrollPane scroll = new JScrollPane(panel);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
-		panel.add(header, BorderLayout.NORTH);
-		panel.add(table, BorderLayout.CENTER);
+        TableRowSorter<TableModel> rowSorter = new TableRowSorter<TableModel>(
+                tablemodel);
+        this.rowSorter = rowSorter;
+        table.setRowSorter(rowSorter);
 
-		JScrollPane scroll = new JScrollPane(panel);
-		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        return scroll;
+    }
 
-		TableRowSorter<TableModel> rowSorter = new TableRowSorter<TableModel>(
-				tableModel);
+    private Component buildSearchTable() {
 
-		this.rowSorter = rowSorter;
+        JPanel panel = new JPanel(new BorderLayout());
+        TableModel tableModel = null; // TODO: fix this add a new class?
+        JTable table = new JTable(tableModel);
+        table.setPreferredSize(panel.getSize());
 
-		table.setRowSorter(rowSorter);
-		return null;
-	}
+        table.setShowGrid(false);
 
-	/**
-	 * Update the row filter regular expression from the expression in the text
-	 * box.
-	 */
-	private void newFilter(TableRowSorter<TableModel> rowSorter,
-			JTextField filterText) {
-		RowFilter<TableModel, Object> rf = null;
-		// If current expression doesn't parse, don't update.
-		try {
-			rf = RowFilter.regexFilter(filterText.getText(), 0);
-		} catch (java.util.regex.PatternSyntaxException e) {
-			return;
-		}
-		rowSorter.setRowFilter(rf);
-	}
+        JTableHeader header = table.getTableHeader();
 
-	public void popup() {
-		pop = new SysadminAnnotationPopup();
-		pop.setBackground(Color.WHITE);
-		JFrame popupFrame = new JFrame("Add new Annotation");
-		popupFrame.setLayout(new BorderLayout());
-		popupFrame.add(pop, BorderLayout.CENTER);
-		popupFrame.pack();
-		popupFrame.setLocationRelativeTo(null);
-		popupFrame.setSize(new Dimension(600, 600));
-		popupFrame.setVisible(true);
-	}
+        panel.add(header, BorderLayout.NORTH);
+        panel.add(table, BorderLayout.CENTER);
 
-	private class SearchDocumentListener implements DocumentListener {
+        JScrollPane scroll = new JScrollPane(panel);
+        scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 
-		private TableRowSorter<TableModel> rowSorter;
-		private JTextField filterText;
+        TableRowSorter<TableModel> rowSorter = new TableRowSorter<TableModel>(
+                tableModel);
 
-		public SearchDocumentListener(TableRowSorter<TableModel> rowSorter,
-				JTextField filterText) {
-			super();
-			this.rowSorter = rowSorter;
-			this.filterText = filterText;
-		}
+        this.rowSorter = rowSorter;
 
-		@Override
-		public void changedUpdate(DocumentEvent e) {
-			newFilter(rowSorter, filterText);
+        table.setRowSorter(rowSorter);
+        return null;
+    }
 
-		}
+    /**
+     * Update the row filter regular expression from the expression in the text
+     * box.
+     */
+    private void newFilter(TableRowSorter<TableModel> rowSorter,
+            JTextField filterText) {
+        RowFilter<TableModel, Object> rf = null;
+        // If current expression doesn't parse, don't update.
+        try {
+            rf = RowFilter.regexFilter(filterText.getText(), 0);
+        } catch (java.util.regex.PatternSyntaxException e) {
+            return;
+        }
+        rowSorter.setRowFilter(rf);
+    }
 
-		@Override
-		public void insertUpdate(DocumentEvent e) {
-			newFilter(rowSorter, filterText);
+    public void popup() {
+        pop = new SysadminAnnotationPopup();
+        pop.setBackground(Color.WHITE);
+        JFrame popupFrame = new JFrame("Add new Annotation");
+        popupFrame.setLayout(new BorderLayout());
+        popupFrame.add(pop, BorderLayout.CENTER);
+        popupFrame.pack();
+        popupFrame.setLocationRelativeTo(null);
+        popupFrame.setSize(new Dimension(600, 600));
+        popupFrame.setVisible(true);
+    }
 
-		}
+    private class SearchDocumentListener implements DocumentListener {
 
-		@Override
-		public void removeUpdate(DocumentEvent e) {
-			newFilter(rowSorter, filterText);
+        private TableRowSorter<TableModel> rowSorter;
+        private JTextField filterText;
 
-		}
+        public SearchDocumentListener(TableRowSorter<TableModel> rowSorter,
+                JTextField filterText) {
+            super();
+            this.rowSorter = rowSorter;
+            this.filterText = filterText;
+        }
 
-	}
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            newFilter(rowSorter, filterText);
 
-	public void addAddAnnotationListener(ActionListener addAnnotationListener) {
-		pop.addAddAnnotationListener(addAnnotationListener);
-	}
-	
-	public void addAddPopupListener(ActionListener addPopupListener) {
-		addButton.addActionListener(addPopupListener);
-	}
-	
-	public String getNewAnnotationName() {
-		return pop.getNewAnnotationName();
-	}
-	
-	public String[] getNewAnnotationCategories(){
-		return pop.getNewAnnotationCategories();
-	}
-	
-	public boolean getNewAnnotationForcedValue(){
-		return pop.getNewAnnotationForcedValue();
-	}
+        }
 
-	public void closePopup() {
-		pop.closeWindow();
-		
-	}
-	
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            newFilter(rowSorter, filterText);
+
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            newFilter(rowSorter, filterText);
+
+        }
+
+    }
+
+    public void addAddAnnotationListener(ActionListener addAnnotationListener) {
+        pop.addAddAnnotationListener(addAnnotationListener);
+    }
+
+    public void addAddPopupListener(ActionListener addPopupListener) {
+        addButton.addActionListener(addPopupListener);
+    }
+
+    public String getNewAnnotationName() {
+        return pop.getNewAnnotationName();
+    }
+
+    public String[] getNewAnnotationCategories() {
+        return pop.getNewAnnotationCategories();
+    }
+
+    public boolean getNewAnnotationForcedValue() {
+        return pop.getNewAnnotationForcedValue();
+    }
+
+    public void closePopup() {
+        pop.closeWindow();
+
+    }
 
 }
