1949a3c6d813c3ee31793d56aef4104d77df5508
bdtkarlsson <bdtkarlsson@hotmail.com>
2014-05-07 08:52:43 +0200

    inget

--- src/gui/sysadmin/AnnotationTableModel.java
+++ src/gui/sysadmin/AnnotationTableModel.java
@@ -6,35 +6,36 @@ import responses.sysadmin.AnnotationData;
 
 public class AnnotationTableModel extends AbstractTableModel {
 
-	private static final long serialVersionUID = 1414328728572140752L;
+    private static final long serialVersionUID = 1414328728572140752L;
 
-	AnnotationData[] annotations;
+    AnnotationData[] annotations;
 
-	@Override
-	public int getRowCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getRowCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public int getColumnCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getColumnCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public Object getValueAt(int rowIndex, int columnIndex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public void setAnnotations(AnnotationData[] annotations) {
-		synchronized (annotations) {
-			this.annotations = annotations;
-		}
+    public void setAnnotations(AnnotationData[] annotations) {
+        synchronized (annotations) {
+            this.annotations = annotations;
+        }
+    }
+    public void setAnnotations() {
 
-	public void setAnnotations() {
-
-	}
+    }
 
 }
+
