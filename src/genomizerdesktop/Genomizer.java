63b9c3ab470a86c4dd53d70680dc69706911a689
worfox <woffie@worfox.se>
2014-05-07 11:42:58 +0200

    Changed URL to the real server

--- src/genomizerdesktop/Genomizer.java
+++ src/genomizerdesktop/Genomizer.java
@@ -33,7 +33,7 @@ public class Genomizer {
 		gui.setWorkspaceTab(wt);
 		gui.setAnalyzeTab(at);
 		gui.setSysAdminTab(sat);
-		Connection con = new Connection("plankarta.cs.umu.se:8080");
+		Connection con = new Connection("scratchy.cs.umu.se:7000");
 		Model model = new Model(con);
 		Controller controller = new Controller(gui, model);
 		gui.showLoginWindow();
