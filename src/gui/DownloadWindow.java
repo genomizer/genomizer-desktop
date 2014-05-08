104c53bb996c05a9a30f7f7f51b2f40d99e96078
Didrik Püschel <diddemon@hotmail.com>
2014-05-08 15:20:39 +0200

    gds

--- src/gui/DownloadWindow.java
+++ src/gui/DownloadWindow.java
@@ -63,7 +63,7 @@ public class DownloadWindow extends JFrame {
         formatConversionColumn.setHeaderValue("Format conversion");
 
         for (int i = 0; i < data.size(); i++) {
-            tableModel.addRow(new Object[]{data.get(i), "Click here to choose"});
+            tableModel.addRow(new Object[]{data.get(i), "Click here to choose file format"});
         }
 
         JComboBox comboBox = new JComboBox();
