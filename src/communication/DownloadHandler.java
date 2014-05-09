7a06fb69013dc83edf3987df7dfa74b69a2a16de
bdtkarlsson <bdtkarlsson@hotmail.com>
2014-05-09 14:30:44 +0200

    asdasf

--- src/communication/DownloadHandler.java
+++ src/communication/DownloadHandler.java
@@ -14,61 +14,62 @@ import org.apache.commons.codec.binary.Base64;
 
 /**
  * Created by Christoffer on 2014-04-30.
- *
+ * 
  */
 public class DownloadHandler {
 
-	private String username;
-	private String password;
+    private String username;
+    private String password;
 
-	public DownloadHandler(String username, String password) {
-		this.username = username;
-		this.password = password;
+    public DownloadHandler(String username, String password) {
+	this.username = username;
+	this.password = password;
+    }
+
+    public boolean download(String url, String localFilePath) {
+	try {
+	    // Use this url in the real version. vvv
+	    /*
+	     * URL targetUrl = new URL(
+	     * "http://scratchy.cs.umu.se:8090/html/download.php?path=" + url);
+	     */
+	    url = url.replaceFirst("\\u003d", "=");
+	    url = url.replaceFirst("scratcy", "scratchy");
+	    url = url.replaceFirst("8090", "8000");
+
+	    URL targetUrl = new URL(url);
+	    String authString = username + ":" + password;
+	    byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+	    String authStringEnc = new String(authEncBytes);
+	    HttpURLConnection conn = (HttpURLConnection) targetUrl
+		    .openConnection();
+	    conn.setRequestMethod("GET");
+	    conn.setRequestProperty("Content-Type", "text/plain");
+	    conn.setRequestProperty("Authorization", "Basic " + authStringEnc);
+	    int responseCode;
+	    responseCode = conn.getResponseCode();
+	    if (responseCode != 200) {
+		System.out
+			.println("Error wrong response code: " + responseCode);
+		return false;
+	    }
+	    System.out.println(responseCode);
+	    BufferedReader in = new BufferedReader(new InputStreamReader(
+		    conn.getInputStream()));
+	    String buffer;
+	    File file = new File(localFilePath);
+	    PrintWriter fileOut = new PrintWriter(new FileWriter(file));
+	    while ((buffer = in.readLine()) != null) {
+		fileOut.println(buffer);
+		fileOut.flush();
+	    }
+	    conn.disconnect();
+
+	} catch (MalformedURLException e) {
+	    e.printStackTrace();
+	} catch (IOException e) {
+	    e.printStackTrace();
 	}
-
-	public boolean download(String url, String localFilePath, String userID) {
-		try {
-			// Use this url in the real version. vvv
-			/*URL targetUrl = new URL(
-					"http://scratchy.cs.umu.se:8090/html/download.php?path="
-							+ url);*/
-            url = url.replaceFirst("\\u003d", "=");
-            url = url.replaceFirst("scratcy", "scratchy");
-            url = url.replaceFirst("8090", "8000");
-
-			URL targetUrl = new URL(url);
-			String authString = username + ":" + password;
-			byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
-			String authStringEnc = new String(authEncBytes);
-			HttpURLConnection conn = (HttpURLConnection) targetUrl
-					.openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("Content-Type", "text/plain");
-			conn.setRequestProperty("Authorization", "Basic " + authStringEnc);
-			int responseCode;
-			responseCode = conn.getResponseCode();
-			if (responseCode != 200) {
-				System.out
-						.println("Error wrong response code: " + responseCode);
-				return false;
-			}
-			System.out.println(responseCode);
-			BufferedReader in = new BufferedReader(new InputStreamReader(
-					conn.getInputStream()));
-			String buffer;
-			File file = new File(localFilePath);
-			PrintWriter fileOut = new PrintWriter(new FileWriter(file));
-			while ((buffer = in.readLine()) != null) {
-				fileOut.println(buffer);
-				fileOut.flush();
-			}
-			conn.disconnect();
-
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return true;
-	}
+	return true;
+    }
 }
