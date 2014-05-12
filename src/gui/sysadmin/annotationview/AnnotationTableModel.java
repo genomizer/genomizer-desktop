4f00ba0490299e8874dfceb3935a4458e626f727
c12jvr <c12jvr@cs.umu.se>
2014-05-12 15:37:40 +0200

    change to login to hankatt

--- src/gui/sysadmin/annotationview/AnnotationTableModel.java
+++ src/gui/sysadmin/annotationview/AnnotationTableModel.java
@@ -8,67 +8,74 @@ import util.AnnotationDataType;
 
 public class AnnotationTableModel extends AbstractTableModel {
 
-    private static final long serialVersionUID = 1414328728572140752L;
+	private static final long serialVersionUID = 1414328728572140752L;
 
-    AnnotationDataType[] annotations = new AnnotationDataType[]{};
+	AnnotationDataType[] annotations = new AnnotationDataType[] {};
 
-    @Override
-    public int getRowCount() {
-        return annotations.length;
-    }
+	@Override
+	public int getRowCount() {
+		return annotations.length;
+	}
 
-    @Override
-    public int getColumnCount() {
-        // TODO Auto-generated method stub
-        return 3;
-    }
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 3;
+	}
 
-    @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		
+		switch (columnIndex) {
+		case 0:
+			return annotations[rowIndex].getName();
+		case 1:
+			StringBuilder string = new StringBuilder("");
+			String[] values = annotations[rowIndex].getValues();
+			if (values != null) {
+				string = new StringBuilder(values[0]);
+				for (int i = 1; i < values.length; i++) {
+					string.append(",").append(values[i]);
+				}
+			} else {
+				System.err.println("Annotation[" + rowIndex
+						+ "].getValues() was null!");
+			}
+			return string;
+		case 2:
+			return annotations[rowIndex].getForced();
+		case 3:
+			return annotations[rowIndex];
+		default:
+			System.err.println("ERROR, should not be able to press here!!!");
+			return null;
+		}
+	}
 
-        switch (columnIndex) {
-            case 0:
-                return annotations[rowIndex].getName();
-            case 1:
-                String[] values = annotations[rowIndex].getValues();
-                StringBuilder string = new StringBuilder(values[0]);
-                for (int i = 1; i <  values.length; i++ ) {
-                    string.append(",").append(values[i]);
-                }
-                return string;
-            case 2:
-                return annotations[rowIndex].getForced();
-            case 3: 
-            	return annotations[rowIndex];
-            default:
-                System.err.println("ERROR, should not be able to press here!!!");
-                return null;
-        }
-    }
-    public void setAnnotations(AnnotationDataType[] annotations) {
-        synchronized (annotations) {
-            this.annotations = annotations;
-        }
-        fireTableDataChanged();
-    }
+	public void setAnnotations(AnnotationDataType[] annotations) {
+		synchronized (annotations) {
+			this.annotations = annotations;
+		}
+		fireTableDataChanged();
+	}
 
-    @Override
-    public String getColumnName(int column) {
-        switch (column) {
-            case 0:
-                return "Name";
-            case 1:
-                return "Types";
-            case 2:
-                return "Forced";
-            default:
-                break;
-        }
-        return null;
+	@Override
+	public String getColumnName(int column) {
+		switch (column) {
+		case 0:
+			return "Name";
+		case 1:
+			return "Types";
+		case 2:
+			return "Forced";
+		default:
+			break;
+		}
+		return null;
 
-    }
+	}
 
-    public AnnotationDataType getAnnotationData(int i) {
-        return annotations[i];
-    }
+	public AnnotationDataType getAnnotationData(int i) {
+		return annotations[i];
+	}
 }
