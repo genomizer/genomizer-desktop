ec8bf65535410c1f1b4599a3c9448923ad660c26
oi11ahn <oi11ahn@voldemort.cs.umu.se>
2014-05-12 11:38:47 +0200

    started to build the users list.

--- src/gui/sysadmin/UsersViewCreator.java
+++ src/gui/sysadmin/UsersViewCreator.java
@@ -1,8 +1,11 @@
 package gui.sysadmin;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class UsersViewCreator {
 
@@ -12,10 +15,22 @@ public class UsersViewCreator {
 
     public JPanel buildUsersView() {
 
-        JPanel mainPanel = new JPanel();
+        JPanel mainPanel = new JPanel(new BorderLayout());
         mainPanel.setBackground(new Color(255, 250, 250));
-
+        mainPanel.add(buildUsersList(), BorderLayout.WEST);
         return mainPanel;
+    }
+
+    private JScrollPane buildUsersList() {
+
+        JScrollPane scrollPane = new JScrollPane();
+        String testme = "HEKJHFRKJEKJ";
+        JList usersList = new JList();
+
+
+        scrollPane.add(usersList);
+
+        return scrollPane;
     }
 
 }
