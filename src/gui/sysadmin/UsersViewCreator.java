963f159299229ab7334917f8b691094c39f84b6e
oi11ahn <oi11ahn@voldemort.cs.umu.se>
2014-05-12 11:15:09 +0200

    Added UsersView tab.

--- src/gui/sysadmin/UsersViewCreator.java
+++ src/gui/sysadmin/UsersViewCreator.java
@@ -1,9 +1,18 @@
 package gui.sysadmin;
 
+import javax.swing.JPanel;
+
 public class UsersViewCreator {
 
     public UsersViewCreator() {
 
+    }
+
+    public JPanel buildUsersView() {
+
+        JPanel mainPanel = new JPanel();
+
+        return mainPanel;
     }
 
 }
