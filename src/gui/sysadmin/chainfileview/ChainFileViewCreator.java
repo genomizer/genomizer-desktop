a083d86b9363c6fe7a7f631e53dea65ac6955ab8
Isak <dv12ilr@cs.umu.se>
2014-05-14 15:54:12 +0200

    added filter JPanel where stuff will be added

--- src/gui/sysadmin/chainfileview/ChainFileViewCreator.java
+++ src/gui/sysadmin/chainfileview/ChainFileViewCreator.java
@@ -17,33 +17,51 @@ public class ChainFileViewCreator {
         return mainPanel;
     }
 
-    private JScrollPane buildChainFileList() {
+    private JScrollPane buildfileList() {
 
-        String[] header = new String[] { "To version", "From version",
-                "File name" };
+        String[] header = new String[] {"From version", "To version",
+                "File name", "Species"};
 
         Object[][] table = new Object[][] {
                 { "Genome release 3.0", "Genome release 1.0",
-                        "randomfilename.txt" },
+                        "randomfilename.txt", "Human"},
                 { "Genome release 4.0", "Genome release 3.0",
-                        "randomfilename.txt" },
+                        "randomfilename.txt", "Human"},
                 { "Genome release 3.0", "Genome release 4.0",
-                        "randomfilename.txt" },
+                        "randomfilename.txt", "Human"},
                 { "Genome release 4.0", "Genome release 5.0",
-                        "randomfilename.txt" },
+                        "randomfilename.txt", "Human"},
                 { "Genome release 5.0", "Genome release 3.0",
-                        "randomfilename.txt" }
+                        "randomfilename.txt", "Human"}
         };
 
         JTable cfTable = new JTable(table, header);
         cfTable.setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
-        cfTable.getTableHeader().setReorderingAllowed(false);
+        //cfTable.getTableHeader().setReorderingAllowed(false);
 
         JScrollPane scrollPane = new JScrollPane(cfTable);
         scrollPane.setPreferredSize(new Dimension(500, 80));
 
         return scrollPane;
     }
+
+    /** TODO: Anna, add your code here!*/
+    private JPanel buildDropDownFilter(){
+        return new JPanel();
+    }
+
+    private JPanel buildChainFileList(){
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        JPanel filterPanel = buildDropDownFilter();
+        JScrollPane tablePanel = buildfileList();
+
+        mainPanel.add(filterPanel, BorderLayout.NORTH);
+        mainPanel.add(tablePanel, BorderLayout.CENTER);
+        return mainPanel;
+    }
+
+
 
     private JPanel buildAddChainFilePanel() {
         JPanel mainPanel = new JPanel();
