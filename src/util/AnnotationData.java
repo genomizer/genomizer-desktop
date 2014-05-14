3ba78c9a41033a3a85a527f8329976b1e1bcb362
c12jvr <c12jvr@cs.umu.se>
2014-05-14 09:57:33 +0200

    Removed id from annotaitons

--- src/util/AnnotationData.java
+++ src/util/AnnotationData.java
@@ -1,11 +1,9 @@
 package util;
 
 public class AnnotationData {
-    public String id;
     public String name;
 
     public AnnotationData(String id, String name) {
-        this.id = id;
         this.name = name;
     }
 
