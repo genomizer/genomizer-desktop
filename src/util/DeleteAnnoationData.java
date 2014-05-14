0d5149db72c3ac265f19338e9992bb808a1e4a4c
Ulrohn <Ulrohn@gmail.com>
2014-05-14 22:16:55 +0200

    Trying to fix deleteannotaion bugg and Yes/No/Unknown bugg

--- src/util/DeleteAnnoationData.java
+++ src/util/DeleteAnnoationData.java
@@ -1,11 +1,11 @@
 package util;
 
 public class DeleteAnnoationData {
-
+    
     public String[] name;
-
+    
     public DeleteAnnoationData(AnnotationDataType annotation) {
-        this.name = new String []{annotation.name};
+        this.name = new String[] { annotation.name };
     }
-
+    
 }
