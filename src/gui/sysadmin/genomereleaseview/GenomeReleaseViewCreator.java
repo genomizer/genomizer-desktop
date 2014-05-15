29f47a50b405544e261eb8d12766a0bea16fc835
Anna <eyra.anna.sofia@gmail.com>
2014-05-15 11:45:33 +0200

    sorted list in the gr table

--- src/gui/sysadmin/genomereleaseview/GenomeReleaseViewCreator.java
+++ src/gui/sysadmin/genomereleaseview/GenomeReleaseViewCreator.java
@@ -53,41 +53,29 @@ public class GenomeReleaseViewCreator {
     public JPanel buildGenomeFileList(){
         JPanel mainPanel = new JPanel(new BorderLayout());
 
-        // String[] header = new String[] { "Genome version", "Species",
-        // "File name"};
-        // Object[][] table = new Object[][] {
-        // { "hg38", "Human", "randomfilename.txt" },
-        // { "hg36", "Human", "randomfilename.txt" },
-        // { "hg32", "Human", "randomfilename.txt" },
-        // { "rn5", "Rat", "randomfilename.txt"}
-        // };
-        
+
         String[] files = { "canfile", "bananafile", "dummyfile", "annafile",
                 "snakefile" };
         String[] versions = { "annaversion", "bananaversion", "dummyversion",
                 "cancanversion", "snakefile" };
         String[] species = { "zebra", "ape", "fly", "dolphin", "dolphin" };
-        
+
         GenomereleaseTableModel tablemodel = new GenomereleaseTableModel();
-        
+
         JTable grTable = new JTable(tablemodel);
         tablemodel.setGenomeReleases(files, species, versions);
         grTable.setShowGrid(false);
-        
+
         TableRowSorter<TableModel> rowSorter = new TableRowSorter<TableModel>(
                 tablemodel);
-        
+
         grTable.setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
         grTable.setRowSorter(rowSorter);
-        
+
         JScrollPane scrollPane = new JScrollPane(grTable);
         JTableHeader header2 = grTable.getTableHeader();
         scrollPane.setPreferredSize(new Dimension(500, 80));
 
-        // JTable cfTable = new JTable(table, header);
-        // cfTable.setSelectionMode(DefaultListSelectionModel.SINGLE_SELECTION);
-        // JScrollPane scrollPane = new JScrollPane(cfTable);
-        //scrollPane.setPreferredSize(new Dimension(500, 80));
 
         mainPanel.add(scrollPane, BorderLayout.CENTER);
         return mainPanel;
