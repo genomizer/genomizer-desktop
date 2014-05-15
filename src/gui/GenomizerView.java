0fc9c8c572228e4271e048ddae354d9dfa1f3110
c11ann <c11ann@gryffindor.cs.umu.se>
2014-05-15 10:07:29 +0200

    fdfd

--- src/gui/GenomizerView.java
+++ src/gui/GenomizerView.java
@@ -134,4 +134,6 @@ public interface GenomizerView {
             ArrayList<ExperimentData> searchResults);
 
     public void enableUploadButton(boolean b);
+
+    public String[] getRatioCalcParameters();
 }
