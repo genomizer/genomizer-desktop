7dc6ba4faa686848375a1a484ec14722524c6b80
c11ann <c11ann@gryffindor.cs.umu.se>
2014-05-16 10:49:50 +0200

    fds

--- src/gui/ProcessTab.java
+++ src/gui/ProcessTab.java
@@ -746,14 +746,6 @@ public class ProcessTab extends JPanel {
 
     }
 
-    public String[] getRatioCalcParameters() {
-        String[] s = new String[2];
-        s[0] = "single 4 0";
-        s[1] = "150 1 7 0 0";
-
-        return s;
-    }
-
     public String[] getOtherParameters() {
         String[] s = new String[2];
         s[0] = "y";
