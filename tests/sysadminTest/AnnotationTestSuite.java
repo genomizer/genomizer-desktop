b7dbddab7426a59d5823833bbf15c5f48c69c2a7
c12jvr <c12jvr@cs.umu.se>
2014-05-16 11:19:20 +0200

    Fixed buggs with add annotation

--- tests/sysadminTest/AnnotationTestSuite.java
+++ tests/sysadminTest/AnnotationTestSuite.java
@@ -1,13 +1,12 @@
 package sysadminTest;
 
-
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GetAnnotationTest.class, AnnotationFieldTest.class, AnnotationValueTest.class, RemoveAnnotationTest.class})
+@SuiteClasses({ GetAnnotationTest.class, AnnotationFieldTest.class,
+        AnnotationValueTest.class, RemoveAnnotationTest.class })
 public class AnnotationTestSuite {
     
 }
