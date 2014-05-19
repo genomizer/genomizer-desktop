5767df8c9672b4c10cff632f7310e9fc81fd644a
c11ann <c11ann@gryffindor.cs.umu.se>
2014-05-19 14:26:24 +0200

    genome file list i process fungerar

--- src/gui/CheckListItem.java
+++ src/gui/CheckListItem.java
@@ -12,18 +12,19 @@ public class CheckListItem {
     private String specie;
     private boolean isSelected = false;
 
-    public CheckListItem(FileData file,ExperimentData exData,String label,String fileId) {
-        this.exData = exData;
+    public CheckListItem(FileData file,String label,String fileId,String specie) {
+        //this.exData = exData;
         this.file = file;
         this.label = label;
         this.fileId = fileId;
 
-        for(util.AnnotationDataValue specie:exData.annotations){
-            if(specie.equals("specie")){
-                this.specie = specie.value;
-                System.out.println("Checklistitem specie: " + specie);
-            }
-        }
+        //for(util.AnnotationDataValue specie:exData.annotations){
+          //  if(specie.equals("specie")){
+            //    this.specie = specie.value;
+              ///  System.out.println("Checklistitem specie: " + specie);
+           // }
+        //}
+        this.specie = specie;
 
         this.label = label + " - " + specie;
     }
