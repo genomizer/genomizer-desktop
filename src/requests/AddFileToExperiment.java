86f3fdb35166f1af438ad1cd5263b078fda32937
oi11ejn <oi11ejn@cs.umu.se>
2014-05-19 10:42:34 +0200

    Comments

--- src/requests/AddFileToExperiment.java
+++ src/requests/AddFileToExperiment.java
@@ -1,10 +1,19 @@
 package requests;
 
 /**
- * Created by worfox on 2014-04-25.
+ * This class represents a "Add a file to an experiment" request in an
+ * application for genome researchers. This request adds a file to an experiment
+ * in the database of the application.
+ *
+ * @author worfox
+ * @date 2014-04-25.
  */
 
 public class AddFileToExperiment extends Request {
+    /**
+     * Attributes needed to create the experiment.
+     *
+     */
     public String experimentID;
     public String fileName;
     public String type;
@@ -14,6 +23,26 @@ public class AddFileToExperiment extends Request {
     public boolean isPrivate;
     public String grVersion;
 
+    /**
+     * Constructor creating the experiment.
+     *
+     * @param experimentId
+     *            String representing the experiment id.
+     * @param fileName
+     *            String representing the file name.
+     * @param type
+     *            String representing the type of the file.
+     * @param metaData
+     *            String representing meta data.
+     * @param author
+     *            String representing the author of the file.
+     * @param uploader
+     *            String representing who uploaded the file.
+     * @param isPrivate
+     *            boolean representing if the file should be private.
+     * @param grVersion
+     *            String representing the genome version of the file.
+     */
     public AddFileToExperiment(String experimentId, String fileName,
             String type, String metaData, String author, String uploader,
             boolean isPrivate, String grVersion) {
--- src/requests/DownloadFileRequest.java
+++ src/requests/DownloadFileRequest.java
@@ -1,7 +1,21 @@
 package requests;
 
+/**
+ * This class represents a "Download file" request in an application for genome
+ * researchers. This request downloads the selected files.
+ *
+ * @author
+ */
 public class DownloadFileRequest extends Request {
 
+    /**
+     * Constructor creating the request.
+     *
+     * @param fileID
+     *            String representing the file id.
+     * @param fileFormat
+     *            String representing the format of the file.
+     */
     public DownloadFileRequest(String fileID, String fileFormat) {
         super("downloadfile", "/file/" + fileID, "GET");
     }
