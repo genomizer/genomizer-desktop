eef9f948082a6f48d260e72bcc339a117d64815c
c12jvr <c12jvr@cs.umu.se>
2014-05-20 15:12:19 +0200

    Fixed annotaitonfield request to the new api.

--- src/requests/RenameAnnotationFieldRequest.java
+++ src/requests/RenameAnnotationFieldRequest.java
@@ -2,12 +2,12 @@ package requests;
 
 public class RenameAnnotationFieldRequest extends Request {
 
-    public String name;
     public String newName;
+    public String oldName;
 
     public RenameAnnotationFieldRequest(String oldname, String newname) {
         super("renameAnnotationRequest", "/annotation/field", "PUT");
-        this.name = oldname;
+        this.oldName = oldname;
         this.newName = newname;
     }
     
