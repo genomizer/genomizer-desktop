c00090969bcdb79594802f5a12a32502d1da84ad
bdtkarlsson <bdtkarlsson@hotmail.com>
2014-05-26 14:31:42 +0200

    warra warra warra warra

--- src/util/Process.java
+++ src/util/Process.java
@@ -4,32 +4,29 @@ import javax.swing.JCheckBox;
 import javax.swing.JTextField;
 
 public class Process {
-
+    
     public boolean isCorrectToProcess(JTextField smoothWindowSize,
             JTextField stepPosition, JTextField stepSize, boolean sgrFormat,
             JCheckBox smoothing, JCheckBox stepBox) {
-        System.out.println("sgrformat: " + sgrFormat);
-        System.out.println("smmothsize: " + smoothWindowSize.getText());
-        System.out.println("steppos: " + stepPosition.getText());
-        System.out.println("stepSize: " + stepSize.getText());
-
+        
         if (!sgrFormat) {
             return true;
         } else if (!smoothing.isSelected()) {
             return true;
         } else if (aboveZero(smoothWindowSize.getText().trim())
                 && aboveZero(stepPosition.getText().trim())) {
-            if (!stepBox.isSelected() || (stepBox.isSelected() && aboveZero(stepSize.getText().trim()))) {
+            if (!stepBox.isSelected()
+                    || (stepBox.isSelected() && aboveZero(stepSize.getText()
+                            .trim()))) {
                 return true;
             }
         }
         return false;
     }
-
+    
     public boolean isRatioCorrectToProcess(JTextField ratioWindowSize,
             JTextField inputReads, JTextField chromosome,
             JTextField ratioStepPosition) {
-        System.out.println("checking if ratio is correct");
         if (aboveZero(ratioWindowSize.getText().trim())
                 && aboveZero(inputReads.getText().trim())
                 && aboveZero(chromosome.getText().trim())
@@ -39,9 +36,9 @@ public class Process {
             return false;
         }
     }
-
+    
     public boolean aboveZero(String string) {
-
+        
         try {
             int value = Integer.parseInt(string);
             if (value >= 0) {
@@ -55,7 +52,7 @@ public class Process {
     }
     
     public boolean ZeroOr(String string) {
-
+        
         try {
             int value = Integer.parseInt(string);
             if (value >= 0) {
@@ -67,5 +64,5 @@ public class Process {
             return false;
         }
     }
-
+    
 }
