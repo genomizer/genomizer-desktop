0e9300629ba932c9ba4ae25acd77b11eba5ab77d
rednatto <rednatto@gmail.com>
2015-04-15 10:27:21 +0200

    Commented a button-creating class

--- src/gui/CustomButtonFactory.java
+++ src/gui/CustomButtonFactory.java
@@ -5,8 +5,25 @@ import java.awt.Dimension;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
+/**
+ * Class (static) capable of creating a custom style button.
+ * <\p>
+ * Can return a JButton with custom image and hover properties. Set up
+ * automatically using 'set...' of the JButton.
+ * @author (of comment) c12oor
+ *
+ */
 public class CustomButtonFactory {
-    
+
+    /**
+     * Get a custom button with given properties.
+     * @param icon ImageIcon to place on the button
+     * @param hoverIcon ImageIcon to use when hovering over the button
+     * @param x Dimension width of buttonsize
+     * @param y Dimension heigth of buttonsize
+     * @param tooltip String of button
+     * @return JButton with these properties set.
+     */
     public static JButton makeCustomButton(ImageIcon icon, ImageIcon hoverIcon, int x, int y, String tooltip) {
         JButton button = new JButton(icon);
         button.setRolloverIcon(hoverIcon);
@@ -18,5 +35,5 @@ public class CustomButtonFactory {
         button.setToolTipText(tooltip);
         return button;
     }
-    
+
 }
