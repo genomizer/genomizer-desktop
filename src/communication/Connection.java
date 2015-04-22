d28da9097b583c44da98cb49da0622e0e00e6fc9
Christopher Fladvad <c12cfd@cs.umu.se>
2015-04-22 09:51:35 +0200

    Added ErrorLogger to respons codes bigger than 300

--- src/communication/Connection.java
+++ src/communication/Connection.java
@@ -114,6 +114,7 @@ public class Connection {
                 return false;
             }
             if (responseCode >= 300) {
+                ErrorLogger.log(responseBody);
                 return false;
             }
             connection.disconnect();
