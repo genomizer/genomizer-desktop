b00c3162b022f6d6023bb96a76fec40033ee4a39
Petter Johansson <oi12pjn@cs.umu.se>
2015-04-27 13:19:56 +0200

    Added constants to index

---
+++ src/util/Constants.java
@@ -0,0 +1,26 @@
+package util;
+/**
+ * A class containing constants for test purposes
+ *
+ *
+ * @author oi12pjn & oi12mlw
+ *
+ */
+public class Constants {
+
+    /**
+     * default username
+     */
+    public static final String userName = "Username";
+    /**
+     * default password
+     */
+    public static final String password = "baguette";
+    /**
+     * default server address
+     */
+    public static final String serverAddress ="130.239.192.110:7000";
+
+
+
+}
