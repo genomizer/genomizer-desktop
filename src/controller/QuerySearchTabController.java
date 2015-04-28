7d9892be72cc051226c47f42765f80eb6543dae6
Viktor Bengtsson <servo_32@hotmail.com>
2015-04-28 13:20:51 +0200

    fixed an error

--- src/controller/QuerySearchTabController.java
+++ src/controller/QuerySearchTabController.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
+
 import model.ErrorLogger;
 import model.GenomizerModel;
 import util.ActiveSearchPanel;
@@ -22,74 +23,75 @@ public class QuerySearchTabController {
     GenomizerView view;
     GenomizerModel model;
     private QuerySearchTab querySearchTab;
-    
+
     public QuerySearchTabController(GenomizerView view, GenomizerModel model) {
         this.view = view;
         this.querySearchTab = view.getQuerySearchTab();
         this.model = model;
-        view.addQuerySearchListener(QuerySearchListener());
-        view.addUpdateSearchAnnotationsListener(updateSearchAnnotationsListener());
-        view.addSearchToWorkspaceListener(SearchToWorkspaceListener());
-        view.addUploadToListenerSearchTab(SearchUploadToListener());
-        
+        view.addQuerySearchListener( QuerySearchListener());
+        view.addUpdateSearchAnnotationsListener( updateSearchAnnotationsListener());
+        view.addSearchToWorkspaceListener( SearchToWorkspaceListener());
+        view.addUploadToListenerSearchTab( SearchUploadToListener());
+
     }
-    
+
     public ActionListener createClearButtonListener() {
         return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                
+
                 querySearchTab.clearSearchFields();
             }
         };
     }
-    
+
     public ActionListener createManualEditButtonListener() {
         return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 querySearchTab.getSearchArea().setEditable(true);
+
                 for (QueryBuilderRow row : querySearchTab.getRowList()) {
                     row.setEnabled(false);
                 }
             }
         };
     }
-    
+
+
+    // En uploadlistener som körs när upload knappen trycks i search-taben
     public ActionListener SearchUploadToListener() {
         return new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    ExperimentData firstChosenExperiment = view
-                            .getSelectedDataInSearch().get(0);
-                    UploadTab ut = view.getUploadTab();
-                    view.getTabbedPane().setSelectedComponent(ut);
-                    ut.getExperimentNameField().setText(
-                            firstChosenExperiment.getName());
-                    ut.getExistingExpButton().doClick();
-                } catch (IndexOutOfBoundsException ee) {
-                    ErrorLogger.log(ee);
-                    JOptionPane.showMessageDialog(null,
-                            "No experiment was selected.");
-                }
-            }
+            public void actionPerformed(ActionEvent e) {            try {
+                ExperimentData firstChosenExperiment = view.getSelectedDataInSearch().get(0);
+                UploadTab ut = view.getUploadTab();
+                view.getTabbedPane().setSelectedComponent(ut);
+                ut.getExperimentNameField().setText(
+                        firstChosenExperiment.getName());
+                ut.getExistingExpButton().doClick();
+            } catch (IndexOutOfBoundsException ee) {
+                ErrorLogger.log(ee);
+                JOptionPane.showMessageDialog(null,
+                        "No experiment was selected.");
+            }}
         };
     }
-    
+
+
     public ActionListener createQueryBuilderButtonListener() {
         return new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 querySearchTab.getSearchArea().setEditable(false);
-                
+
                 for (QueryBuilderRow row : querySearchTab.getRowList()) {
                     row.setEnabled(true);
                 }
             }
         };
     }
-    
+
     public ActionListener createBackButtonListener() {
         return new ActionListener() {
             @Override
@@ -98,81 +100,73 @@ public class QuerySearchTabController {
             }
         };
     }
-    
-    public ActionListener QuerySearchListener() {
-        return new ActionListener() {
-            
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Thread() {
-                    @Override
-                    public void run() {
-                        String pubmed = view.getQuerySearchString();
-                        ArrayList<ExperimentData> searchResults = model
-                                .search(pubmed);
-                        if (searchResults != null) {
-                            view.updateQuerySearchResults(searchResults);
-                            
-                            // If search results are null and the active panel
-                            // is search
-                        } else if (view.getActiveSearchPanel() == ActiveSearchPanel.SEARCH) {
-                            JOptionPane.showMessageDialog(null,
-                                    "No search results!", "Search Warning",
-                                    JOptionPane.WARNING_MESSAGE);
-                            
-                            // If search results are null and the active panel
-                            // is table
-                        } else if (view.getActiveSearchPanel() == ActiveSearchPanel.TABLE) {
-                            // Go back to the query search
-                            view.getBackButton().doClick();
-                        }
-                    }
-                }.start();
-            }
-        };
-    }
-    
-    public ActionListener updateSearchAnnotationsListener() {
-        return new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                
-                new Thread() {
-                    @Override
-                    public void run() {
-                        AnnotationDataType[] annotations = model
-                                .getAnnotations();
-                        if (annotations != null && annotations.length > 0) {
-                            view.setSearchAnnotationTypes(annotations);
-                        }
-                    };
-                }.start();
-            }
-        };
-    }
-    
-    public ActionListener SearchToWorkspaceListener() {
-        return new ActionListener() {
-            
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new Thread() {
-                    @Override
-                    public void run() {
-                        ArrayList<ExperimentData> selectedData = view
-                                .getSelectedDataInSearch();
-                        if (selectedData != null && selectedData.size() > 0) {
-                            view.addToWorkspace(view.getSelectedDataInSearch());
-                            view.changeTabInWorkspace(0);
-                        }
-                        view.clearSearchSelection();
-                    };
-                }.start();
-            }
-        };
-        
-    }
-    
+public ActionListener QuerySearchListener() {
+    return new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread() {
+                @Override
+                public void run() {
+                    String pubmed = view.getQuerySearchString();
+                    ArrayList<ExperimentData> searchResults = model.search(pubmed);
+                    if (searchResults != null) {
+                        view.updateQuerySearchResults(searchResults);
 
-    
+                        // If search results are null and the active panel is search
+                    } else if (view.getActiveSearchPanel() == ActiveSearchPanel.SEARCH) {
+                        JOptionPane.showMessageDialog(null, "No search results!",
+                                "Search Warning", JOptionPane.WARNING_MESSAGE);
+
+                        // If search results are null and the active panel is table
+                    } else if (view.getActiveSearchPanel() == ActiveSearchPanel.TABLE) {
+                        // Go back to the query search
+                        view.getBackButton().doClick();
+                    }
+                };
+            }.start();
+        }
+    };
+}
+
+public ActionListener updateSearchAnnotationsListener() {
+    return new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread() {
+                @Override
+                public void run() {
+                    AnnotationDataType[] annotations = model.getAnnotations();
+                    if (annotations != null && annotations.length > 0) {
+                        view.setSearchAnnotationTypes(annotations);
+                    }
+                
+                };
+            }.start();
+        }
+    };
+}
+
+public ActionListener SearchToWorkspaceListener() {
+    return new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            new Thread() {
+                @Override
+                public void run() {
+                    ArrayList<ExperimentData> selectedData = view
+                            .getSelectedDataInSearch();
+                    if (selectedData != null && selectedData.size() > 0) {
+                        view.addToWorkspace(view.getSelectedDataInSearch());
+                        view.changeTabInWorkspace(0);
+                    }
+                    view.clearSearchSelection();
+                };
+            }.start();
+        }
+    };
+}
+
+
+
+
 }
