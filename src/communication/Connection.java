e20188dcaafeadda9207ceaf5dd16cd017c8e5dd
Jonas Hedin <c12jhn@cs.umu.se>
2015-04-29 09:41:03 +0200

    Changed back to http

--- src/communication/Connection.java
+++ src/communication/Connection.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
+import javax.net.ssl.HttpsURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
