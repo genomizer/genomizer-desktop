551a0f145ac486c192df4bf63e13775124697a99
oi12mlw <oi12mlw@cs.umu.se>
2015-05-13 14:01:12 +0200

    Removed unused imports.

--- src/gui/ErrorDialog.java
+++ src/gui/ErrorDialog.java
@@ -5,12 +5,8 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
-import java.awt.KeyEventDispatcher;
-import java.awt.KeyboardFocusManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
