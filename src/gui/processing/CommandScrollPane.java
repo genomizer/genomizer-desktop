5b6169fa4f1d6e884423678d23125e8024564420
oi12mlw <oi12mlw@cs.umu.se>
2015-05-26 10:24:58 +0200

    You can no longer remove the last file row from a command component

--- src/gui/processing/CommandScrollPane.java
+++ src/gui/processing/CommandScrollPane.java
@@ -1,44 +1,81 @@
 package gui.processing;
 
+import java.awt.Rectangle;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+
+import javax.swing.BoxLayout;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 @SuppressWarnings("serial")
 public class CommandScrollPane extends JScrollPane {
-
+    
+    
+    private static final int SCROLL_SPEED = 16;
+    private JPanel componentPanel;
+    
     public CommandScrollPane() {
         super(VERTICAL_SCROLLBAR_AS_NEEDED, HORIZONTAL_SCROLLBAR_NEVER);
+        
+        componentPanel = new JPanel();
+        componentPanel.setLayout(new BoxLayout(componentPanel, BoxLayout.PAGE_AXIS));
+        this.getViewport().add(componentPanel);
+        this.getVerticalScrollBar().setUnitIncrement(SCROLL_SPEED);
+        
+        setUpAutoScrollBehaviour();
+        
     }
 
-    public void addCommandComponent(RawToProfileCommandComponent commandComponent) {
-        this.getViewport().add(commandComponent);
+    private void setUpAutoScrollBehaviour() {
+        this.getVerticalScrollBar().addAdjustmentListener(new AdjustmentListener() {            
+            int prevMax = 0;
+            public void adjustmentValueChanged(AdjustmentEvent e) {
+                if(e.getAdjustable().getMaximum() != prevMax) {
+                    prevMax = e.getAdjustable().getMaximum();
+                    e.getAdjustable().setValue(prevMax);                    
+                } 
+            }
+        });
+    }
+    
+    public void addCommandComponent(
+            RawToProfileCommandComponent commandComponent) {
+        componentPanel.add(commandComponent);
+        Rectangle bounds = new Rectangle(0, componentPanel.getHeight() - 1 , 1, 1);
+        this.scrollRectToVisible(bounds);
         this.revalidate();
     }
-
+    
     public static void main(String[] args) {
-
+        
         try {
             UIManager
                     .setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
         } catch (ClassNotFoundException | InstantiationException
                 | IllegalAccessException | UnsupportedLookAndFeelException e) {
         }
-
+        
         JFrame frame = new JFrame();
         String commandName = "Glass e gott";
-        String[] fileNames = {"bild.png", "stinkern.txt", "hus.jpg"};
-        String[] genomeReleases = {"hg37", "hg38", "rat"};
-
-        RawToProfileCommandComponent comp1 = new RawToProfileCommandComponent(commandName, fileNames, genomeReleases);
-
+        String[] fileNames = { "bild.png", "stinkern.txt", "hus.jpg" };
+        String[] genomeReleases = { "hg37", "hg38", "rat" };
+        
+        RawToProfileCommandComponent comp1 = new RawToProfileCommandComponent(
+                commandName, fileNames, genomeReleases);
+        RawToProfileCommandComponent comp2 = new RawToProfileCommandComponent(
+                commandName, fileNames, genomeReleases);
+        
         CommandScrollPane c = new CommandScrollPane();
         c.addCommandComponent(comp1);
+        c.addCommandComponent(comp2);
         frame.add(c);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
     }
-
+    
 }
--- src/gui/processing/RawToProfileCommandComponent.java
+++ src/gui/processing/RawToProfileCommandComponent.java
@@ -2,6 +2,7 @@ package gui.processing;
 
 import gui.CustomButtonFactory;
 
+import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
@@ -26,85 +27,91 @@ import util.IconFactory;
 
 @SuppressWarnings("serial")
 public class RawToProfileCommandComponent extends JComponent {
-
+    
     private String commandName;
     private String[] fileNames;
     private String[] genomeReleases;
-
+    
     private JButton newFileRowButton;
-
+    private int fileRowCount = 0;
+    
     private HashMap<JButton, JPanel> removeButtonToPanelMap = new HashMap<JButton, JPanel>();
-
-    public RawToProfileCommandComponent(String commandName, String[] fileNames, String[] genomeReleases) {
-
+    
+    public RawToProfileCommandComponent(String commandName, String[] fileNames,
+            String[] genomeReleases) {
+        
         this.commandName = commandName;
         this.fileNames = fileNames;
         this.genomeReleases = genomeReleases;
-
+        
         this.setBorder(new TitledBorder(commandName));
         this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-
+        
         addFileRow();
     }
-
-
+    
     private void addFileRow() {
-
+        fileRowCount++;
         JPanel fileRowPanel = new JPanel();
         fileRowPanel.setLayout(new FlowLayout());
-
+        
         RawToProfileFileRow fileRow = new RawToProfileFileRow(fileNames, genomeReleases);
-
-
-        JPanel removeButtonPanel = new JPanel(new GridLayout(2,1));
-
+        
+        JPanel removeButtonPanel = new JPanel(new GridLayout(2, 1));
         JButton removeButton = buildRemoveButton();
         removeButton.addActionListener(new RemoveButtonListener());
         removeButtonToPanelMap.put(removeButton, fileRowPanel);
-
+        
         JPanel paddingPanel = new JPanel();
         paddingPanel.setPreferredSize(new Dimension(17, 17));
         removeButtonPanel.add(paddingPanel);
         removeButtonPanel.add(removeButton);
-
+        
         fileRowPanel.add(fileRow);
         fileRowPanel.add(removeButtonPanel);
-
+        
+        if (fileRowCount < 2) {
+            removeButton.setEnabled(false);
+            removeButton.setIcon(null);
+        }
+        
         this.add(fileRowPanel);
         addNewFileRowButton();
         this.repaint();
         this.revalidate();
-
+        
     }
-
-
+    
     private JButton buildRemoveButton() {
         ImageIcon icon = IconFactory.getMinusIcon(15, 15);
         ImageIcon hoverIcon = IconFactory.getMinusIcon(17, 17);
         int width = 17;
         int height = 25;
         String tooltip = "Remove this file row";
-        return CustomButtonFactory.makeCustomButton(icon, hoverIcon, width, height, tooltip);
+        return CustomButtonFactory.makeCustomButton(icon, hoverIcon, width,
+                height, tooltip);
     }
-
+    
     private JButton buildNewRowButton() {
         ImageIcon icon = IconFactory.getPlusIcon(15, 15);
         ImageIcon hoverIcon = IconFactory.getPlusIcon(17, 17);
         int width = 17;
         int height = 25;
         String tooltip = "Add a new file row";
-        return CustomButtonFactory.makeCustomButton(icon, hoverIcon, width, height, tooltip);
+        return CustomButtonFactory.makeCustomButton(icon, hoverIcon, width,
+                height, tooltip);
     }
-
+    
     private void removeFileRow(Object source) {
         JPanel fileRowToRemove = removeButtonToPanelMap.get(source);
         this.remove(fileRowToRemove);
         removeButtonToPanelMap.remove(source);
         this.repaint();
         this.revalidate();
-
+        fileRowCount--;
+        
     }
-
+    
     private void addNewFileRowButton() {
         if (newFileRowButton != null) {
             this.remove(newFileRowButton);
@@ -113,23 +120,23 @@ public class RawToProfileCommandComponent extends JComponent {
         newFileRowButton.addActionListener(new NewFileRowButtonListener());
         this.add(newFileRowButton);
     }
-
+    
     private class RemoveButtonListener implements ActionListener {
-
+        
         @Override
         public void actionPerformed(ActionEvent e) {
             removeFileRow(e.getSource());
         }
-
+        
     }
-
+    
     private class NewFileRowButtonListener implements ActionListener {
-
+        
         @Override
         public void actionPerformed(ActionEvent e) {
             addFileRow();
         }
-
+        
     }
-
+    
 }
